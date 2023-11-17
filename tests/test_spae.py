f44b21cbe554e0cad62eb91070aea08e9bb6f184
Bhavya Nalagampalli Papudeshi <npbhavya13@gmail.com>
2023-11-17 16:50:05 +1030

    vijini is a genius

--- tests/test_spae.py
+++ tests/test_spae.py
@@ -47,11 +47,21 @@ def test_spae_run_help(tmp_dir):
     exec_command("spae run -h")
 
 def test_spae_config_help(tmp_dir):
-    exec_command("spae config -h")
+    exec_command("spae config -h")    
 
 def test_spae_illumina(tmp_dir):
-    exec_command(f"spae run --input {TEST_ROOTDIR}/data/illumina-subset --output {tmp_dir} --touch")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/Pfam35.0/ && touch {EXEC_ROOTDIR}/spae/workflow/databases/Pfam35.0/Pfam-A.hmm.gz")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/pharokka_db && touch {EXEC_ROOTDIR}/spae/workflow/databases/pharokka_db/phrogs_db.index")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/checkv-db-v1.5 && touch {EXEC_ROOTDIR}/spae/workflow/databases/checkv-db-v1.5/README.txt")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/phynteny_models_zenodo && touch {EXEC_ROOTDIR}/spae/workflow/databases/phynteny_models_zenodo/grid_search_model.m_400.b_256.lr_0.0001.dr_0.1.l_2.a_tanh.o_rmsprop.rep_0.best_val_loss.h5")
+    exec_command(f"spae run --input {TEST_ROOTDIR}/data/illumina-subset --output {tmp_dir} --touch -n")
+    exec_command(f"rm -rf {EXEC_ROOTDIR}/spae/workflow/databases/")
+
 
 def test_spae_longread(tmp_dir):
-    exec_command(f"spae run --input {TEST_ROOTDIR}/data/nanopore-subset --output {tmp_dir} --sequencing longread --touch")
-
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/Pfam35.0/ && touch {EXEC_ROOTDIR}/spae/workflow/databases/Pfam35.0/Pfam-A.hmm.gz")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/pharokka_db && touch {EXEC_ROOTDIR}/spae/workflow/databases/pharokka_db/phrogs_db.index")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/checkv-db-v1.5 && touch {EXEC_ROOTDIR}/spae/workflow/databases/checkv-db-v1.5/README.txt")
+    exec_command(f"mkdir -p {EXEC_ROOTDIR}/spae/workflow/databases/phynteny_models_zenodo && touch {EXEC_ROOTDIR}/spae/workflow/databases/phynteny_models_zenodo/grid_search_model.m_400.b_256.lr_0.0001.dr_0.1.l_2.a_tanh.o_rmsprop.rep_0.best_val_loss.h5")
+    exec_command(f"spae run --input {TEST_ROOTDIR}/data/nanopore-subset --output {tmp_dir} --sequencing longread --touch -n")
+    exec_command(f"rm -rf {EXEC_ROOTDIR}/spae/workflow/databases/")
