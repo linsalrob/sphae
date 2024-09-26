d4852adf612bccc61204821e98c0debb58937b71
npbhavya <npbhavya@gmail.com>
2024-09-26 10:50:34 +0930

    i have no idea what changes have been made

--- sphae.sh
+++ sphae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=sphae-medaka
+#SBATCH --job-name=sphae
 #SBATCH --mail-type=ALL
 #SBATCH --output=%x-%j.out.txt
 #SBATCH --error=%x-%j.err.txt
@@ -13,7 +13,7 @@
 
 #sphae install
 #sphae run --input tests/data/illumina-subset --threads 64 -k 
-#sphae run --input tests/data/nanopore-subset --sequencing longread --threads 64 -k
+#sphae run --input /home/nala0006/scratch/Achromobacter/phage/fastq --sequencing longread --threads 64 -k --output /home/nala0006/scratch/Achromobacter/phage/sphae-outv1.4.4
 #sphae run --input tests/data/nanopore-subset --sequencing longread --threads 64 -k --no_medaka 
-#sphae annotate --genome tests/data/genome --threads 64
+sphae annotate --genome /home/nala0006/scratch/Achromobacter/phage/fasta2 --threads 64 --output /home/nala0006/scratch/Achromobacter/phage/2089-annot-v1.4.4
 
