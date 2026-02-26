79bf6776c77d5a0305842233aa6dfa5bad670dd3
npbhavya <npbhavya13@gmail.com>
2026-02-26 12:17:26 +0100

    updated pharokka yaml

--- sphae.sh
+++ sphae.sh
@@ -18,6 +18,6 @@ export PHOLDDB=/users/bnalaga1/scratch/reference_db/sphae_databases/phold
 sphae run --input tests/data/illumina-subset --threads 32 -k
 sphae run --input tests/data/nanopore-subset --sequencing longread --threads 64 -k
 sphae run --input tests/data/nanopore-subset --sequencing longread --threads 64 -k --no_medaka  
-sphae annotate --genome tests/data/genome --threads 32 -k
+sphae annotate --genome /users/bnalaga1/scratch/soil_metagenomes/Comb_phage_genomes --threads 32 -k --profile slurm
 #singularity exec -B /home/nala0006/scratch/sphae/sphae/workflow/databases:/databases,/home/nala0006/scratch/sphae/tests/data/illumina-subset:/input,/home/nala0006/scratch/sphae/output:/output /home/nala0006/scratch/docker/sphae_v1.4.8.sif  sphae run --input /input --output /output
 
